39cc4619ebde97e8b6a35fc25475a9110b41d426
Tare <grillzwitu@gmail.com>
2024-03-27 16:34:52 +0100

    Created radio buttons

--- hello.py
+++ hello.py
@@ -43,11 +43,43 @@ root.iconbitmap("./assets/icons/currency_exchange_finance_coins_money_usd_dollar
 
 #create an exit button
 btn_exit = Button(root, text="Exit", command=root.quit)
-btn_exit.grid(row=7, column=1)
+btn_exit.grid(row=10, column=1)
 
 #Images
 img1 = ImageTk.PhotoImage(Image.open("./assets/images/apple_pic.jpg"))
 img_label = Label(image=img1)
 img_label.grid(row=5, column=1)
+
+#Frames
+
+frame = LabelFrame(root, text="A simple frame", padx=5, pady=5)
+frame.grid(row=6, column=0, padx=10, pady=10)
+
+frm_btn = Button(frame, text="Button in a Frame")
+frm_btn.grid(row=0, column=0)
+
+#Radio Button
+
+treat_select = StringVar(value="None")
+
+def  selected(value):
+    """
+    Faciclitates the selection of a radio button
+    """
+    global selection
+
+    selection.grid_forget()
+
+    selection = Label(frame, text=value)
+    selection.grid(row=3, column=1)
+
+rdio_btn1 = Radiobutton(frame, text="cake", variable=treat_select, value="cake", command= lambda: selected(treat_select.get()))
+rdio_btn2 = Radiobutton(frame, text="cookie", variable=treat_select, value="cookie", command= lambda: selected(treat_select.get()))
+rdio_btn1.grid(row=1, column=1)
+rdio_btn2.grid(row=2, column=1)
+
+
+selection = Label(frame)
+selection.grid(row=3, column=1)
 
 root.mainloop()
